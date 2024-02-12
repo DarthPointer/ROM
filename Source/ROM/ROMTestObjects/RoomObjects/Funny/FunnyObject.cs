6ca8d5ac8f8834e0af365e32bae3e73df15a107b
Inevitabilis <ddd1020018@gmail.com>
2024-02-12 21:31:20 +0300

    JSON loop reference fix

--- Source/ROM/ROMTestObjects/RoomObjects/Funny/FunnyObject.cs
+++ Source/ROM/ROMTestObjects/RoomObjects/Funny/FunnyObject.cs
@@ -18,6 +18,7 @@ namespace ROMTestObjects.RoomObjects.Funny
         public int PlusFiveMinusThreeInt { get; set; }
         public int PlusFiveMinusThreeInt2 { get; set; }
         public char CursedChar { get; set; }
+        [JsonIgnore]
         public Vector2[] Polygon { get; set; } =
         {
             new Vector2(500, 500),
@@ -25,6 +26,27 @@ namespace ROMTestObjects.RoomObjects.Funny
             new Vector2(600, 600),
             new Vector2(600, 500)
         };
+        public SerializableVector2[] SerializablePolygon
+        {
+            get
+            {
+                return [
+                    new SerializableVector2(Polygon[0]),
+                    new SerializableVector2(Polygon[1]),
+                    new SerializableVector2(Polygon[2]),
+                    new SerializableVector2(Polygon[3])
+                ];
+            }
+            set
+            {
+                Polygon = [
+                    new Vector2(value[0].x, value[0].y),
+                    new Vector2(value[1].x, value[1].y),
+                    new Vector2(value[2].x, value[2].y),
+                    new Vector2(value[3].x, value[3].y),
+                ];
+            }
+        }
 
         public bool AFlag { get; set; }
 
@@ -53,6 +75,11 @@ namespace ROMTestObjects.RoomObjects.Funny
         {
             this.x = x;
             this.y = y;
+        }
+        public SerializableVector2(Vector2 v)
+        {
+            this.x = v.x;
+            this.y = v.y;
         }
         
     }
